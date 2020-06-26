3f3b7d331f6e380f13ac2620b76287ec7cf02ed0
Terra <terra.taylor@gmail.com>
2020-06-26 08:42:00 -0600

    init commit

---
+++ repos.js
@@ -0,0 +1,54 @@
+/**
+ * Fetch user repos
+ */
+const getRepos = function() {
+  fetch('https://api.github.com/users/andreacardybailey/repos')
+    .then(response => response.json())
+    // 👇 You MUST work with the data HERE 👇
+    .then(jsonData => {
+      extractData(jsonData);
+    })
+    .catch(error => console.log(error));
+};
+
+/**
+ * Extract data to be used on page
+ * @param data - the JSON data
+ */
+const extractData = function(data){
+  data.forEach(repo => {
+    let {
+      name,
+      html_url,
+      created_at,
+      description
+    } = repo;
+    
+    let dateCreated = new Date(created_at);
+    $('.repos').append(createTemplate(name, html_url, dateCreated, description));
+  });
+};
+
+/**
+ * Create HTML template for each result
+ * @param {string} repo_name
+ * @param {string} url
+ * @param {Date} created_at
+ * @param {string} description
+ */
+const createTemplate = function(repo_name, url, date, description) {
+  let template = `
+  <section>
+    <h2><a href="${url}">${repo_name}</a></h2>
+    <ul>
+      <li>Description: ${description}</li>
+      <li>
+        Date created: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}
+      </li>
+    </ul>
+  </section>
+  `;
+  return template;
+};
+
+$(getRepos);
